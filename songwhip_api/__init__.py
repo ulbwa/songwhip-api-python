a1fa30b52efd65977d2c8ec3604ce0fd0f634754
ульба <ulbwa@icloud.com>
2024-02-28 17:26:18 +0500

    minor fixes

--- songwhip_api/__init__.py
+++ songwhip_api/__init__.py
@@ -3,16 +3,16 @@ import random
 import urllib.parse
 import urllib.parse
 from datetime import timedelta
-from typing import List
+from typing import List, Union, Optional
 
 import orjson
 from aiohttp_client_cache import CachedSession, FileBackend, CacheBackend
 from aiohttp_proxy import ProxyConnector
 from fake_headers import Headers
 
-logger = logging.getLogger("songwhip_api")
-
 from songwhip_api.types import *
+
+logger = logging.getLogger("songwhip_api")
 
 
 class SongWhip:
@@ -23,19 +23,17 @@ class SongWhip:
     api_url: str
     api_timeout: int
     always_use_proxy: bool
-    cache_backend: CacheBackend | None
-    use_orjson: bool
+    cache_backend: Optional[CacheBackend]
 
-    __connections: List[ProxyConnector | None]
+    __connections: List[Optional[str]]
 
     def __init__(
         self,
         api_url: str = "https://songwhip.com/",
         api_timeout: int = 60,
-        proxy: List[ProxyConnector] | ProxyConnector | None = None,
+        proxy: Optional[Union[List[str], str]] = None,
         always_use_proxy: bool = False,
-        cache_backend: CacheBackend
-        | None = FileBackend(
+        cache_backend: Optional[CacheBackend] = FileBackend(
             expire_after=timedelta(days=31),
             ignored_parameters=["key"],
             allowed_codes=(200,),
@@ -43,26 +41,22 @@ class SongWhip:
             cache_control=False,
             use_temp=True,
         ),
-        use_orjson: bool = True,
-    ) -> None:
+    ):
         """
         Initialize the SongWhip instance.
 
         :param api_url: The base URL of the SongWhip API.
         :param api_timeout: Timeout for API requests in seconds.
-        :param proxy: Proxy connector(s) to use for requests.
+        :param proxy: Proxy(s) to use for requests.
         :param always_use_proxy: Flag indicating whether to always use the provided proxy.
         :param cache_backend: Cache backend to use for caching API responses.
-        :param use_orjson: Flag indicating whether to use orjson for JSON serialization.
         """
         self.api_url = api_url.rstrip("/")
         self.api_timeout = api_timeout
-        self.proxy = proxy
         self.always_use_proxy = always_use_proxy
         self.cache_backend = cache_backend
-        self.use_orjson = use_orjson
 
-        self.__connections: List[ProxyConnector | None] = []
+        self.__connections = []
         if proxy is not None:
             for _proxy in proxy if isinstance(proxy, list) else [proxy]:
                 self.__connections.append(_proxy)
@@ -87,8 +81,11 @@ class SongWhip:
 
         :return: An instance of Album, Track, or Artist.
         """
+
+        connection = random.choice(self.__connections)
+
         async with CachedSession(
-            connector=random.choice(self.__connections),
+            connector=ProxyConnector.from_url(connection) if connection else None,
             cache=self.cache_backend,
         ) as session:
             async with session.post(
@@ -103,10 +100,12 @@ class SongWhip:
             ) as response:
                 try:
                     raw_data = orjson.loads(await response.read())
-                    logger.debug("Got response on URL %s: %s", url, raw_data)
+                    logger.debug("Got response on URL %s: %s", response.url, raw_data)
                     data = raw_data["data"]["item"]
-                except Exception as e:
-                    raise APIException(status_code=response.status, message=str(e))
+                except Exception as exception:
+                    raise APIException(
+                        status_code=response.status, message=str(exception)
+                    ).with_traceback(exception.__traceback__)
 
             if response.status != 200 or data == {}:
                 raise APIException(
